4a3494dfa9a6831b18110afd65415d9d2be54eab
AchiniPramoda <it20125066@my.sliit.lk>
2023-03-12 09:47:04 +0530

    Add files

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:exerciseapp/Screens/UserManagement/Signin.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:exerciseapp/Screens/UserManagement/Signin.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+   await FirebaseAppCheck.instance.activate(
+    webRecaptchaSiteKey: 'recaptcha-v3-site-key',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+       debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple
+      ),
+      home: (SignInScreen()),
+    );
+  }
+}
